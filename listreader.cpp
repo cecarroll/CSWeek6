1da79516401ab0aa2750e9c1bd9bcee95a714c33
cecarroll <cecarrollwork@gmail.com>
2025-10-15 18:38:11 +0000

    this puppy is done now. there was a list of things wrong. remember to specify the c++ edition bc apparently that just can change.

--- listreader.cpp
+++ listreader.cpp
@@ -16,30 +16,39 @@ int main(){
 	std::stringstream converter;
 	std::string wholeThing;
 	std::string word;
-	bool keepGoing;
+	bool keepGoing = true;
 	
 	std::cout << "Hello World!"<< std::endl;
 	std::ifstream inFile;
   	inFile.open("data.csv");
 	while ( keepGoing ) {
-		if (std::getline(inFile, wholeThing) ){
+		if (!std::getline(inFile, wholeThing)){
 			keepGoing = false;
 		}else{
-			std::cout<<"read successful"<<std::endl;
+			//std::cout<<"read successful"<<std::endl;
 			converter.clear();
 			converter.str(wholeThing);	
 			std::getline(converter, sfirstInt, ',');
 			std::getline(converter, ssecondInt, ',');
 			std::getline(converter, word);
-
-			std::cout << sfirstInt << " " << ssecondInt<< " " << std::endl;
+			//std::cout << sfirstInt << " " << ssecondInt<< " " << word <<std::endl;
 			
-		}
-	
-	
-	}
+		}// end if
+		firstInt = std::stoi(sfirstInt); 
+		secondInt = std::stoi(ssecondInt);
+		comboInt = firstInt + secondInt;
+		for(int i  = 0; i < comboInt; i++ ){
+			std::cout <<word << " " ; 		
+
+
+
+		}//end for loop
+		std::cout << std::endl; 
+		word = "";
+		
+	} //end while loop
 	
 
-}
+} // end main 
 
 
